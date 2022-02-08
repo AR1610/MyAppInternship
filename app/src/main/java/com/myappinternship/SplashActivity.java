03cc519f72c19444f758bb505c63a2e31a287043
Aarti Patel <royal.arati10@gmail.com>
2022-02-08 09:45:15 +0530

    Types of Intent & Image Pick from Gallery (OnActivityResulit), SplashActivity with GIF imageview

---
+++ app/src/main/java/com/myappinternship/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.myappinternship;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    int time = 3000;
+    GifImageView gifImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        gifImageView = findViewById(R.id.img_gif);
+        gifImageView.setGifImageResource(R.drawable.android);
+    new Handler().postDelayed(new Runnable() {
+        @Override
+        public void run() {
+            Intent i = new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(i);
+            finish();
+        }
+    },time);
+
+    }
+}
