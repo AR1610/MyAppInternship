fa56647319f6376890c9797aecd14f92cb6a95a0
Aarti Patel <royal.arati10@gmail.com>
2022-02-18 08:34:40 +0530

    Custom Dialog

--- app/src/main/java/com/myappinternship/MainActivity.java
+++ app/src/main/java/com/myappinternship/MainActivity.java
@@ -1,21 +1,26 @@
 package com.myappinternship;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText edtEmail;
-    Button btnLogin,btnSend;
+    Button btnLogin, btnSend;
     ImageView imgLogo;
+    TextView tvfp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +28,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         edtEmail = findViewById(R.id.edt_email);
         btnLogin = findViewById(R.id.btn_login);
-        imgLogo  = findViewById(R.id.img_logo);
+        imgLogo = findViewById(R.id.img_logo);
         btnSend = findViewById(R.id.btn_send);
+        tvfp = findViewById(R.id.tv_fp);
+
+        tvfp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View fpView = layoutInflater.inflate(R.layout.raw_fp, null);
+                EditText edtFPEmail = fpView.findViewById(R.id.edt_email);
+                Button btnSubmit = fpView.findViewById(R.id.btn_submit);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                AlertDialog alertDialog = builder.create();
+                alertDialog.setView(fpView);
+                alertDialog.show();
+
+                btnSubmit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "Submit", Toast.LENGTH_SHORT).show();
+
+                        if (alertDialog.isShowing()) {
+
+                            alertDialog.dismiss();
+                        }
+                    }
+                });
+            }
+        });
 
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.google.com/"));
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/"));
                 startActivity(i);
             }
         });
@@ -38,14 +71,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String strEmail = edtEmail.getText().toString();
-                if (strEmail.equals("")){
+                if (strEmail.equals("")) {
                     Toast.makeText(MainActivity.this, "Enter Email ID ", Toast.LENGTH_SHORT).show();
-                }else {
-                    Toast.makeText(MainActivity.this, "Email ID is  "+strEmail, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Email ID is  " + strEmail, Toast.LENGTH_SHORT).show();
                     imgLogo.setImageResource(R.drawable.icon_2);
                     // Explicit Intent
-                    Intent  i = new Intent(MainActivity.this,HomeActivity.class);
-                    i.putExtra("KEY_EMAIL",strEmail);
+                    Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                    i.putExtra("KEY_EMAIL", strEmail);
                     startActivity(i);
                     finish();
                     // Over Explicit Intent
